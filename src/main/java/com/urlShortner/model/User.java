e87f25a5bef11548dac5abd9611db220248c60ad
benpointdex <185585972+benpointdex@users.noreply.github.com>
2025-12-31 17:23:48 +0530

    created the db models

---
+++ src/main/java/com/urlShortner/model/User.java
@@ -0,0 +1,21 @@
+package com.urlShortner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+    private String roles="ROLE_USER";
+}
