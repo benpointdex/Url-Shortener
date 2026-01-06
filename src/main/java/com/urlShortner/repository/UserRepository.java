f6e5c00385444886c221ea39944f1afef7dd18d8
benpointdex <185585972+benpointdex@users.noreply.github.com>
2026-01-07 01:06:42 +0530

    jwt setup is done!!

---
+++ src/main/java/com/urlShortner/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.urlShortner.repository;
+
+import com.urlShortner.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/urlShortner/service/UserDetailsImpl.java
@@ -0,0 +1,52 @@
+package com.urlShortner.service;
+
+import com.urlShortner.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID=1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(User user){
+        GrantedAuthority authority= new SimpleGrantedAuthority(user.getRoles());
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singleton(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public  String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
